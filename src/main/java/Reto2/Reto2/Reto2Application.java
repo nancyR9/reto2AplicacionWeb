5f94f1b38ca6f6da140646796b175d47c53d0f23
nancyR9 <nancy.cicua.mt@correo.usa.edu.co>
2021-12-12 20:54:37 -0500

    ajuste run

--- src/main/java/Reto2/Reto2/Reto2Application.java
+++ src/main/java/Reto2/Reto2/Reto2Application.java
@@ -1,13 +1,28 @@
 package Reto2.Reto2;
 
+import Reto2.Reto2.interfaces.InterfaceCleaningProduct;
+import Reto2.Reto2.interfaces.InterfaceUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Reto2Application {
+public class Reto2Application implements CommandLineRunner {
 
+    
+    @Autowired
+    private InterfaceCleaningProduct interfaceCleanProd;
+      @Autowired
+      private InterfaceUser interfaceUser;
 	public static void main(String[] args) {
 		SpringApplication.run(Reto2Application.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        interfaceCleanProd.deleteAll();
+        interfaceUser.deleteAll();
+    }
 
 }
